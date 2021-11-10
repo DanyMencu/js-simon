6161bb60963d0c9de23b7b358bfa35ee0a462f4f
DanyMencu <dany69men@gmail.com>
2021-11-10 14:13:49 +0100

    Create first output numbers whit 2second delay

--- script.js
+++ script.js
@@ -5,3 +5,34 @@
 //*2-l’utente deve inserire, UNO ALLA VOLTA, i numeri che ha visto precedentemente, tramite il prompt().
 
 //*Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//Ref
+const indexText = document.querySelector('.text');
+
+//Random numbers array
+const randomNumbers = [];
+
+setTimeout(() => {
+    //HTML cleaning
+    indexText.innerHTML = '';
+
+    for (let i = 0; i < 5; i++) {
+        randomNumbers.push( randomNum(1, 10) );
+    }
+
+    //Output numbers
+    indexText.innerHTML = randomNumbers;
+
+    console.log(randomNumbers);
+}, 2000)
+
+
+
+
+/* 
+*FUNCTIONS
+*/
+
+function randomNum(min, max) {
+    return Math.floor( Math.random() *(max - min + 1) + min );
+};
