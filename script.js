b01231d12abb6f0f160d7e0bb329da38541c4026
DanyMencu <dany69men@gmail.com>
2021-11-10 14:47:39 +0100

    Add point 2 and 3

--- script.js
+++ script.js
@@ -6,11 +6,13 @@
 
 //*Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+//* 1.
 //Ref
 const indexText = document.querySelector('.text');
 
 //Random numbers array
 const randomNumbers = [];
+const playerNumbers = [];
 
 setTimeout(() => {
     //HTML cleaning
@@ -24,8 +26,32 @@ setTimeout(() => {
     indexText.innerHTML = randomNumbers;
 
     console.log(randomNumbers);
-}, 2000)
 
+    //Second part of the game (remember the numbers)
+    setTimeout(() => {
+        //* 2.
+        indexText.innerHTML = '';
+        setTimeout(() => {
+            //* 3.
+            let promptNumber = 0;
+            do {
+                promptNumber++;
+
+                const playerChoice = ( parseInt( prompt(`Inserire uno alla volta i numeri che si ricordano. ${promptNumber} di ${randomNumbers.length}`) ) );
+
+                playerNumbers.push(playerChoice);
+            }   while (promptNumber < randomNumbers.length - 1) {
+                promptNumber++;
+
+                playerNumbers.push( parseInt( prompt(`Inserire uno alla volta i numeri che si ricordano. ${promptNumber} di ${randomNumbers.length}`) ) );
+            };
+            console.log('I numeri del giocatore sono: ',playerNumbers);
+
+        }, 500);
+
+    },2500);
+
+}, 1500);
 
 
 
